8a22e24b0576d2a18670e3f3132f9c7920617fa9
Nitesh Chaudhary <niteshchaudharywdn@gmail.com>
2023-09-08 20:18:17 +0530

    fisrt commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Abc() {
+        return <h1>THIS IS HEADIGN</h1>
+}
+export function Asd(){
+        return <p>This is name export function</p>
+}
+export default Abc;
+export function Abv(){
+        return <p><h2>THIS CREATE AND REACT APP</h2>
+        <h3>THE CREATE REACT JA</h3></p>
+}
